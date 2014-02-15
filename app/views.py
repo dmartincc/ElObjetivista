364c86bee7311efce774db5b6f8ee8766ef0fae4
David <ultimoresorte@hotmail.com>
2014-02-15 20:12:14 +0100

    newLayOut

--- app/views.py
+++ app/views.py
@@ -1,21 +1,48 @@
 from flask import render_template
 from flask import jsonify
 from app import app
-from getNews import getData
+import getNews 
 import json
+import time, random
+
+input = getNews.getData()
+jsonData = json.loads(input) 
+news  = jsonData['value']['items'] 
+ 
 
 @app.route('/')
-def index():
-    input = getData()
-    #input = open('app/jsonOutput.json','r')
-    #jsonData = json.loads(input.read())
-    #input.close()
-    jsonData = json.loads(input)    
-    news  = jsonData['value']['items']
+def index():     
+    now = time.strftime("%c")
+    range=len(news)-30
+    data=random.sample(news, 100)
+    print data
     return render_template("index.html",
                             #title = 'Home',
-                            news = news)
+                            news = data[-10:],
+                            entities = news,
+                            now= now)
 
+@app.route('/analytics')
+def analytics(): 
+    words=[]
+    for art in news:
+        if getNews.whatisthis(art['title']) == "unicode string":
+            words_title=art['title'].encode('utf-8','ignore').split() 
+        else: 
+            words_title=str(art['title']).split()        
+        words.extend(words_title)
 
+    wordFreqD = {}   
+    sum = 0
+    for i in range(len(words)):
+        for char in '@%#?.!/;:<",':  
+            words[i] = words[i].replace(char,'')
+        wordFreqD[words[i]] = wordFreqD.get(words[i], 0)+1
+
+    output = [{k:v} for v,k in sorted([(v,k) for k,v in wordFreqD.items()],reverse=True)]
+
+    return str(output)    
+
+   
     
 
