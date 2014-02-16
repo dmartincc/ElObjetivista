9744f52d6314bc7568f58fb65a8fca3048c68dd9
David <ultimoresorte@hotmail.com>
2014-02-16 20:37:16 +0100

    d3

--- app/views.py
+++ app/views.py
@@ -4,48 +4,56 @@ from app import app
 import getNews 
 import json
 import time, random
+import re
+from nltk.corpus import stopwords
 
-
+input = getNews.getData()
+jsonData = json.loads(input) 
+news  = jsonData['value']['items'] 
  
 
 @app.route('/')
-def index(): 
-    input = getNews.getData()
-    jsonData = json.loads(input) 
-    news  = jsonData['value']['items'] 
+def index():     
     now = time.strftime("%c")
     range=len(news)-30
     data=random.sample(news, 100)
-    print data
     return render_template("index.html",
                             #title = 'Home',
                             news = data[-10:],
-                            entities = news,
+                            entities = data,
                             now= now)
 
 @app.route('/analytics')
 def analytics(): 
-    input = getNews.getData()
-    jsonData = json.loads(input) 
-    news  = jsonData['value']['items']
+    now = time.strftime("%c")
     words=[]
     for art in news:
         if getNews.whatisthis(art['title']) == "unicode string":
-            words_title=art['title'].encode('utf-8','ignore').split() 
+            words_title=art['title'].encode('utf-8','ignore').replace("@%#()&?.!/;':\"<,",'').split()
         else: 
-            words_title=str(art['title']).split()        
+            words_title=str(art['title']).split()       
         words.extend(words_title)
-
+    
+    #This is the simple way to remove stop words
+    important_words=[]
+    for word in words:
+        if word.lower() not in stopwords.words('spanish'):
+            important_words.append(word)
+        
     wordFreqD = {}   
     sum = 0
-    for i in range(len(words)):
-        for char in '@%#?.!/;:<",':  
-            words[i] = words[i].replace(char,'')
-        wordFreqD[words[i]] = wordFreqD.get(words[i], 0)+1
+    for i in range(len(important_words)):
+        for char in '\"':  
+            important_words[i] = important_words[i].replace(char,'')
+        wordFreqD[important_words[i]] = wordFreqD.get(important_words[i], 0)+1
 
-    output = [{k:v} for v,k in sorted([(v,k) for k,v in wordFreqD.items()],reverse=True)]
+    output = [{"children":{"packageName":"entities","className":k,"value":v}} for v,k in sorted([(v,k) for k,v in wordFreqD.items()],reverse=True)]
+    output2=output[:20]
+    return render_template("analytics.html",                            
+                            entities = output2,
+                            now= now)
 
-    return str(output)    
+       
 
    
     
