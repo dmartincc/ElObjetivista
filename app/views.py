152bea0cb7f2e4bc6d107c89efd8e438d0623d56
David <ultimoresorte@hotmail.com>
2014-09-05 18:15:34 +0200

    changes

--- app/views.py
+++ app/views.py
@@ -1,11 +1,6 @@
-from flask import render_template
-from flask import jsonify
+from flask import render_template, jsonify
 from app import app
-import getNews 
-import json
-import time, random
-import re
-from nltk.corpus import stopwords
+import getNews , json, time, random, re
 
 @app.route('/')
 def index(): 
@@ -23,16 +18,16 @@ def index():
 
 @app.route('/test')
 def index2(): 
-    #input = getNews.fetchApi('http://127.0.0.1:5000/api/0.1/articles')  
-    #jsonData = json.loads(input) 
-    #news  = jsonData['result']
+    input = getNews.fetchApi('http://www.elobjetivista.com/api/0.1/articles')  
+    jsonData = json.loads(input) 
+    news  = jsonData['result']
 
-    input = getNews.fetchNews()
+    """input = getNews.fetchNews()
     jsonData = json.loads(input) 
     news  = jsonData['value']['items'] 
     now = time.strftime("%c")
     range=len(news)-30
-    data=random.sample(news, range)
+    data=random.sample(news, range)"""
     
     a = ['a','b','c','d']
     for item in news:
